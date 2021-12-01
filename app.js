ea0e210dcf834cbead7bb90105378b5d6537cca4
Erinn Schneider <erinnaschneider@gmail.com>
2021-12-01 12:13:39 -0500

    generated html file with fs module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
+const fs = require('fs');
+
 const generatePage = (name, github) => {
     return `
     <!DOCTYPE html> 
@@ -19,5 +21,8 @@ const generatePage = (name, github) => {
     `;
   };
 
-  console.log(name, github);
-  console.log(generatePage(name, github));
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
