735c5bdbee5593bb3e65b627af9d17ff9529efd5
Erinn Schneider <erinnaschneider@gmail.com>
2021-12-01 08:34:14 -0500

    generate html template

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-
-// Notice the lack of parentheses around the profileDataArr parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const [name, github] = profileDataArgs;
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
   
-  printProfileData(profileDataArgs);
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+  console.log(name, github);
+  console.log(generatePage(name, github));
